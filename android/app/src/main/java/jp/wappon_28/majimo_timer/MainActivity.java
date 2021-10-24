4edd81cde816d051eb799ab1c96f4ba8ef73eddf
wappon_28_dev <86721991+wappon-28-dev@users.noreply.github.com>
2021-10-25 02:18:21 +0900

    delete all

--- android/app/src/main/java/jp/wappon_28/majimo_timer/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package jp.wappon_28.majimo_timer;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
--- android/app/src/main/kotlin/jp/wappon28dev/majimo_timer/majimo_timer/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package jp.wappon_28_dev.majimo_timer
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
