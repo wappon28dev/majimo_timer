fcc2901ea9f6bd92f94f4ddf3fd85ac9dbd377da
wappon_28_dev <86721991+wappon-28-dev@users.noreply.github.com>
2022-01-28 03:26:09 +0900

    material_you_adaptive

--- lib/helper/config.dart
+++ lib/helper/config.dart
@@ -1,14 +1,12 @@
-const String version = 'majimo_timer v0.1.8 β';
-const String buildDate = '2022/01/26 1:55';
+const String version = 'majimo_timer v0.1.9 β';
+const String buildDate = '2022/01/27 1:32';
 const String changeLog = '''
 [fix]
-  1. Renamed all freezed class:
-      'Manager'   ->  'State'
-      'ManagerVM' ->  'Controller'
-    This was inspired by MVC Architecture
-  2. Directory of '/lib' structure was improved
-  3. CountDownController has been included Controller class
-
+  1. android 12 material you icon created
+  2. rive (splash screen) updated
+  3. launcher icon: png -> svg
+  4. remade adaptive icon
+  
 [known-bug]
   1. Overlay TK screen always applied
   2. native splash appears twice on A12
